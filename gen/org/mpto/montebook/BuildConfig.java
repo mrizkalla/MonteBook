3cf9f8a4b2b9b36f808d2966df7e99048673840a
Michael Rizkalla <mrizkalla@gmail.com>
2013-11-17 23:43:49 -0800

    Initial app

---
+++ gen/org/mpto/montebook/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.mpto.montebook;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/org/mpto/montebook/AuthActivity.java
@@ -0,0 +1,16 @@
+package org.mpto.montebook;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class AuthActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle arg0) {
+		// TODO Auto-generated method stub
+		setContentView(R.layout.authenticate);
+
+		super.onCreate(arg0);
+	}
+
+}

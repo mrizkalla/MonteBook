e3694eda9cca81e6480cd453be2f072ed1de2fab
Michael Rizkalla <mrizkalla@gmail.com>
2013-11-19 08:08:55 -0800

    Simple List 1

--- src/org/mpto/montebook/StaffDirectoryFragment.java
+++ src/org/mpto/montebook/StaffDirectoryFragment.java
@@ -1,17 +1,41 @@
 package org.mpto.montebook;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class StaffDirectoryFragment extends Fragment {
+public class StaffDirectoryFragment extends ListFragment {
+	private View fragment;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		return inflater.inflate(R.layout.staff_directory, container, false);
+		fragment = inflater.inflate(R.layout.staff_directory, container, false);
+		
+		ArrayList<String> items = new ArrayList<String>();
+		items.add("Item 1");
+		items.add("Item 2");
+		items.add("Item 3");
+		items.add("Item 4");
+		items.add("Item 5");
+		items.add("Item 6");
+		items.add("Item 7");
+		items.add("Item 8");
+		ArrayAdapter<String> aItems = new ArrayAdapter<String>(this.getActivity(), R.layout.list_item, items);
+		
+		setListAdapter(aItems);
+		
+		
+		
+		
+		return fragment;
 	}
 }
